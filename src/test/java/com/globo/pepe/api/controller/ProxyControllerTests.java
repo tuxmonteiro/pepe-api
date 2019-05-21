164212b16065283472252ab51fd0906ca6fcd11b
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-05-21 15:17:40 -0300

    Tests: Improve ProxyControllerTests

--- src/test/java/com/globo/pepe/api/controller/ProxyControllerTests.java
+++ src/test/java/com/globo/pepe/api/controller/ProxyControllerTests.java
@@ -16,6 +16,7 @@
 
 package com.globo.pepe.api.controller;
 
+import com.globo.pepe.api.configuration.RestTemplateConfiguration;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -33,10 +34,11 @@ import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest({ProxyController.class, RestTemplate.class})
+@WebMvcTest({ProxyController.class, RestTemplateConfiguration.class, RestTemplate.class})
 public class ProxyControllerTests {
 
     private static ClientAndServer mockServer;
@@ -48,22 +50,22 @@ public class ProxyControllerTests {
     public static void setupClass() throws IOException {
         mockServer = ClientAndServer.startClientAndServer(9000);
 
-        mockServer.when(request().withMethod("POST").withPath("/auth/testauth").withBody("{\"path\":\"auth\"}"))
+        mockServer.when(request().withMethod("POST").withPath("/admin/auth/testauth").withBody("{\"path\":\"auth\"}"))
             .respond(response().withBody("{\"path\":\"auth\"}").withHeader("Content-Type", APPLICATION_JSON_VALUE).withStatusCode(201));
 
-        mockServer.when(request().withMethod("POST").withPath("/api/testapi").withBody("{\"path\":\"api\"}"))
+        mockServer.when(request().withMethod("POST").withPath("/admin/api/testapi").withBody("{\"path\":\"api\"}"))
             .respond(response().withBody("{\"path\":\"api\"}").withHeader("Content-Type", APPLICATION_JSON_VALUE).withStatusCode(201));
 
-        mockServer.when(request().withMethod("POST").withPath("/stream/teststream").withBody("{\"path\":\"stream\"}"))
+        mockServer.when(request().withMethod("POST").withPath("/admin/stream/teststream").withBody("{\"path\":\"stream\"}"))
             .respond(response().withBody("{\"path\":\"stream\"}").withHeader("Content-Type", APPLICATION_JSON_VALUE).withStatusCode(201));
 
-        mockServer.when(request().withMethod("GET").withPath("/auth/testauth"))
+        mockServer.when(request().withMethod("GET").withPath("/admin/auth/testauth"))
                 .respond(response().withBody("{\"path\":\"auth\"}").withHeader("Content-Type", APPLICATION_JSON_VALUE).withStatusCode(200));
 
-        mockServer.when(request().withMethod("GET").withPath("/api/testapi"))
+        mockServer.when(request().withMethod("GET").withPath("/admin/api/testapi"))
                 .respond(response().withBody("{\"path\":\"api\"}").withHeader("Content-Type", APPLICATION_JSON_VALUE).withStatusCode(200));
 
-        mockServer.when(request().withMethod("GET").withPath("/stream/teststream"))
+        mockServer.when(request().withMethod("GET").withPath("/admin/stream/teststream"))
                 .respond(response().withBody("{\"path\":\"stream\"}").withHeader("Content-Type", APPLICATION_JSON_VALUE).withStatusCode(200));
 
     }
@@ -75,9 +77,11 @@ public class ProxyControllerTests {
 
 
     @Test
-    public void apiPostTest() throws Exception {
-        mockMvc.perform(post("/api/testapi").content("{\"path\":\"api\"}")
-                .contentType(APPLICATION_JSON_VALUE)).andExpect(status().isCreated());
+    public void proxyPostTest() throws Exception {
+        mockMvc.perform(post("/admin/api/testapi").content("{\"path\":\"api\"}")
+                .contentType(APPLICATION_JSON_VALUE))
+                .andExpect(status().isCreated())
+                .andDo(print());
     }
 
 }
