1ea4b19f1985f7cc4146c8bec8887048e6faa9c9
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-05-21 15:14:02 -0300

    Logging: add zalando support

---
+++ src/main/java/com/globo/pepe/api/configuration/HttpLogFormatterConfiguration.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2019 - Globo.com - ATeam
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.globo.pepe.api.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.Correlation;
+import org.zalando.logbook.HttpLogFormatter;
+import org.zalando.logbook.HttpRequest;
+import org.zalando.logbook.HttpResponse;
+import org.zalando.logbook.JsonHttpLogFormatter;
+import org.zalando.logbook.Precorrelation;
+
+@Configuration
+public class HttpLogFormatterConfiguration {
+
+    @Value("${pepe.logging.tags}")
+    private String loggingTags;
+
+    @Bean
+    public HttpLogFormatter jsonFormatter(final ObjectMapper mapper) {
+        return new PrincipalHttpLogFormatter(new JsonHttpLogFormatter(mapper));
+    }
+
+    private class PrincipalHttpLogFormatter implements HttpLogFormatter {
+
+        private final JsonHttpLogFormatter delegate;
+
+        public PrincipalHttpLogFormatter(JsonHttpLogFormatter delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public String format(Precorrelation<HttpRequest> precorrelation) throws IOException {
+            final Map<String, Object> content = delegate.prepare(precorrelation);
+            content.put("short_message", "Request message");
+            content.put("tags", loggingTags);
+            return delegate.format(content);
+        }
+
+        @Override
+        public String format(Correlation<HttpRequest, HttpResponse> correlation) throws IOException {
+            final Map<String, Object> content = delegate.prepare(correlation);
+            content.put("body_response", correlation.getOriginalResponse().getBodyAsString());
+            content.put("short_message", "Response message");
+            content.put("tags", loggingTags);
+            return delegate.format(content);
+        }
+    }
+}
