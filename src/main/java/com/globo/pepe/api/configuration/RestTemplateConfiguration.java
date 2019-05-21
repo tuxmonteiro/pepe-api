29f5e044b99f4dbba00c3f6923d85430f52d1a79
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-05-21 15:36:22 -0300

    RestTemplate: Disable SSL check

--- src/main/java/com/globo/pepe/api/configuration/RestTemplateConfiguration.java
+++ src/main/java/com/globo/pepe/api/configuration/RestTemplateConfiguration.java
@@ -16,8 +16,12 @@
 
 package com.globo.pepe.api.configuration;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -25,7 +29,10 @@ public class RestTemplateConfiguration {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLHostnameVerifier(new NoopHostnameVerifier()).build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        return new RestTemplate(requestFactory);
     }
 
 }
