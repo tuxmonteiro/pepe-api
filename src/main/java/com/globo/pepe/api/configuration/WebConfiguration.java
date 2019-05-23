db8326399c4988179133f704a7c7e293f1109b02
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-05-23 15:05:34 -0300

    Add CORS configuration

---
+++ src/main/java/com/globo/pepe/api/configuration/WebConfiguration.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 - Globo.com - ATeam
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.globo.pepe.api.configuration;
+
+import java.util.stream.Stream;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Value("${pepe.api.origins}")
+    private String pepeOrigin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(pepeOrigin)
+            .allowedMethods(Stream.of(HttpMethod.values()).map(HttpMethod::name).toArray(String[]::new))
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
+}
