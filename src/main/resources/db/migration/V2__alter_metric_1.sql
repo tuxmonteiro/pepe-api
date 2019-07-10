651ca8afd8c7c222223bdc7b158b659ea5177c81
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-07-10 18:26:04 -0300

    Change V2 DB Schema

--- src/main/resources/db/migration/V2__alter_metric_1.sql
+++ src/main/resources/db/migration/V2__alter_metric_1.sql
@@ -2,8 +2,8 @@ SET autocommit=0;
 START TRANSACTION;
 
 ALTER TABLE `metric` ADD COLUMN `last_processing` datetime(6) NULL;
-ALTER TABLE `metric` ADD COLUMN `interval` bigint(20) UNSIGNED DEFAULT 30;
-ALTER TABLE `metric` ADD COLUMN `enable` int(1) UNSIGNED NOT NULL DEFAULT TRUE;
+ALTER TABLE `metric` ADD COLUMN `interval_time` bigint(20) UNSIGNED DEFAULT 30;
+ALTER TABLE `metric` ADD COLUMN `metric_enable` int(1) UNSIGNED NOT NULL DEFAULT TRUE;
 
 COMMIT;
 SET autocommit=1;
